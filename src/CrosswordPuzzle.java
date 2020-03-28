82e1738f8f9c2e72705c90e1de232b10de5d84e3
Esme <esmepuzio@gmail.com>
2020-03-28 11:10:27 -0700

    Completed crossword puzzle

--- src/CrosswordPuzzle.java
+++ src/CrosswordPuzzle.java
@@ -1,5 +1,30 @@
 
-public class CrosswordPuzzle
-{
-	
+public class CrosswordPuzzle {
+	private Square[][] puzzle;
+
+	public CrosswordPuzzle(boolean[][] blackSquares) {
+		int labelCounter = 1;
+		puzzle = new Square[blackSquares.length][blackSquares[0].length];
+		for (int row = 0; row < blackSquares.length; row++) {
+			for (int column = 0; column < blackSquares.length; column++) {
+				if (blackSquares[row][column] == true) {
+					puzzle[row][column] = new Square(true, 0);
+				} else {
+					if (toBeLabeled(row, column, blackSquares) == true) {
+						puzzle[row][column] = new Square(false, labelCounter);
+						labelCounter++;
+					} else {
+						puzzle[row][column] = new Square(false, 0);
+					}
+				}
+			}
+		}
+	}
+
+	private boolean toBeLabeled(int r, int c, boolean[][] blackSquares) {
+		if ((r == 0 || c == 0) || blackSquares[r - 1][c] == true || blackSquares[r][c - 1] == true) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,6 @@
 public class Square{
-
+	private boolean isBlack;
+	public Square(boolean isBlack, int num) {
+		this.isBlack = isBlack;
+	}
 }
